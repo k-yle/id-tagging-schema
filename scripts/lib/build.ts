6dcd2ee173902f9730236fd9e191b61589a266cf
Kyle Hensel <k.y.l.e@outlook.co.nz>
2026-03-05 22:51:15 +1100

    iDTS#1538 add definitions for the structure of 33 relation presets

--- scripts/lib/build.ts
+++ scripts/lib/build.ts
@@ -470,7 +470,6 @@ function generatePresets(
     if (preset.relationCrossReference) {
       references.presets[id] ||= {};
       references.presets[id].relation = preset.relationCrossReference;
-      delete preset.relationCrossReference;
     }
 
     if (preset.locationSet) {
